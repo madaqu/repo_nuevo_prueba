e5c1eed68ddefc86af955dad7cbd766667627139
“Marcelo <alumnomaster2@tecnica.local>
2023-03-22 20:21:13 -0300

    subo dos nuevos archivos

---
+++ Ej1.py
@@ -0,0 +1,20 @@
+a= False and True
+print(a)
+b=True or False
+print(b)
+c=True and True or False
+print(c)
+d=not True and True or False and False or True
+print(d)
+e=False or False and True or True and not False
+print(e)
+f=False or True and True or True and not False
+print(f)
+g=True or True and True or True and not False or False or True and True or True and not False
+print(g)
+h=not True or True and True or True and not False or False or True and True or True and not False
+print(h)
+i=not(not(not(False)))
+print(i)
+j=not(not(not(not(True))))
+print(j)
